af3c0cfa917d9ccd5f0468eaead1c0c9f1b7eba0
MattJB2 <bodnark.2@osu.edu>
2025-04-18 22:08:02 -0400

    Permanently applied tonemap shader

--- src/main/java/matt/tonemap/Tonemap.java
+++ src/main/java/matt/tonemap/Tonemap.java
@@ -1,19 +1,20 @@
-package turniplabs.examplemod;
+package matt.tonemap;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class Tonemap implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+    public static final String MOD_ID = "tonemap";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("Tonemap initialized.");
     }
 
 	@Override
@@ -33,6 +34,8 @@ public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEn
 
 	@Override
 	public void afterGameStart() {
+		//get hijacked lmao
+		Minecraft.getMinecraft().setRenderer(new TonemappingShadersRenderer(Minecraft.getMinecraft()));
 
 	}
 }
