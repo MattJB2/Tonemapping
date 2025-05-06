859995049e361ab990eb20c1b968d83761bd411f
MattJB2 <bodnark.2@osu.edu>
2025-05-06 01:46:12 -0400

    Implemented Photo Mode and Screenshotting

--- src/main/java/matt/tonemap/mixin/PhotoModeMixin.java
+++ src/main/java/matt/tonemap/mixin/PhotoModeMixin.java
@@ -3,18 +3,11 @@ package matt.tonemap.mixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenPhotoMode;
 import net.minecraft.client.render.shader.PhotoModeRenderer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import net.minecraft.client.util.helper.ScreenShot;
 
 import matt.tonemap.TonemapPhotoModeRenderer;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.io.File;
 
 @Mixin(ScreenPhotoMode.class)
 public abstract class PhotoModeMixin {
